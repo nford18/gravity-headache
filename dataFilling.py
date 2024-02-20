17ffb153be06213994f42b6608de083d4709e052
Nick <nickford185@gmail.com>
2024-02-20 12:25:44 -0500

    changed data processing to only output mass from physical properties (along with the orbital properties)

--- dataFilling.py
+++ dataFilling.py
@@ -2,79 +2,74 @@ import math
 import matplotlib.pyplot as plt
 import numpy.random as rand
 
-# d_arr = []
-
-
 with open("./cappy/data/fullData.csv", "w+") as new_file:
     # file has order: "e","a","i","om","w","diameter","H","albedo"
+    new_file.write("\'e\', \'a\', \'i\', \'om\', \'w\', \'mass\',\n")
     with open("./cappy/data/source_data.csv") as file:
         file.readline() # skip header line
         data = file.readlines()
         for index, line in enumerate(data):
             lineArr = line.strip("\n").split(",")
-            newline = ["0: type","1: e","2: a","3: i","4: om","5: w",
-                    "6: diameter","7: H","8: albedo","9: density"]
+            newline = ["0: e","1: a","2: i","3: om","4: w",
+                    "5: diameter"]
             hadDiameter = lineArr[5] != ""
             hadAlbedo = lineArr[7] != ""
-            missingH = lineArr[6] == "" # apparently there are 5 blank H values
+            missingH = lineArr[6] == "" # apparently there can be blank H values
 
             # populate new line with orbital parameters
-            newline[1] = lineArr[0] # e
-            newline[2] = lineArr[1] # a
-            newline[3] = lineArr[2] # i
-            newline[4] = lineArr[3] # om
-            newline[5] = lineArr[4] # w
+            newline[0] = lineArr[0] # e
+            newline[1] = lineArr[1] # a
+            newline[2] = lineArr[2] # i
+            newline[3] = lineArr[3] # om
+            newline[4] = lineArr[4] # w
             if(missingH):
                 print("line", index, "skipped due to missing H-value.")
             else:
-                # populate new line with H
-                newline[7] = lineArr[6]
                 if(hadAlbedo):
-                    # populate new line with given albedo
-                    newline[8] = lineArr[7]
+                    # grab given albedo
+                    albedo = float(lineArr[7])
                     # find type from albedo
-                    if (float(lineArr[7])==0.03):
-                        newline[0] = "C"
-                    elif (float(lineArr[7]) > 0.18):
-                        newline[0] = "S"
+                    if (albedo==0.03):
+                        astType = "C"
+                    elif (albedo > 0.18):
+                        astType = "S"
                     else:
                         # if [0.1, 0.18), pick S or M based on rarity distribution
                         if(rand.uniform(0.0,25.0) <=17.0):
-                            newline[0] = "S"
+                            astType = "S"
                         else:
-                            newline[0] = "M"
+                            astType = "M"
                 else:
                     # find albedo from a random type
                     randNum = rand.uniform(0.0, 1.0)
                     if(randNum <= 0.75):   # 75% C
-                        newline[0] = "C"
-                        newline[8] = str(0.03)
+                        astType = "C"
+                        albedo = str(0.03)
                     elif(randNum <= 0.92): # 17% S
-                        newline[0] = "S"
-                        newline[8] = rand.uniform(0.1,0.22)
+                        astType = "S"
+                        albedo = rand.uniform(0.1,0.22)
                     else:                 # 8% M
-                        newline[0] = "M"
-                        newline[8] = rand.uniform(0.1,0.18)
+                        astType = "M"
+                        albedo = rand.uniform(0.1,0.18)
                 if (hadDiameter):
-                    # populate new line with given diameter
-                    newline[6] = lineArr[5]
-                    # d_arr.append(float(lineArr[1]))
+                    # grab given diameter
+                    diameter = float(lineArr[5])
                 else:
-                    # populate new line with derived diameter
-                    # print(lineArr[6], "->", newline[7])
-                    # print("<",newline[7],">", " <",newline[8] ,">")
-                    newline[6] = str(10**(-float(newline[7])/5.0) / math.sqrt(float(newline[8])) * 1329)
-                    
+                    # derive diameter
+                    diameter = 10**(-float(lineArr[6])/5.0) / math.sqrt(float(albedo)) * 1329
                         
                 # get density from type
-                if (newline[0] == "C"):
-                    newline[9] = str(rand.uniform(3.111069652, 3.441218905, 1)[0])
-                elif (newline[0] == "S"):
-                    newline[9] = str(rand.uniform(3.183649635, 3.444379562, 1)[0])
+                if (astType == "C"):
+                    density = rand.uniform(3.111069652, 3.441218905)
+                elif (astType == "S"):
+                    density = rand.uniform(3.183649635, 3.444379562)
                 else:
-                    newline[9] = str(rand.uniform(6.73375, 6.87875, 1)[0])
-                # print(newline)
-                new_file.write(str(newline).strip("[]\'") + "\n")
+                    density = rand.uniform(6.73375, 6.87875)
+                
+                newline[5] = str(math.pi/6.0 * math.pow(diameter,3) *density)
+                for element in newline:
+                    new_file.write(element.strip("[]\'") + ", ")
+                new_file.write("\n")
 """
 // Graphing diameter for testing 
 print("Valid points found: " + str(len(d_arr)))
