59857a9e83eb6632d101dfa41f52bd115385508e
Nick Ford <nickford185@gmail.com>
2024-03-19 10:26:50 -0400

    continued on filling in c, moved first file reference in filling py, and added bisection to asteroid model

--- dataFilling.c
+++ dataFilling.c
@@ -1,10 +1,22 @@
-#include <cmath>
-#include <cstdio>
-#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 /*
  * File may be a new version of dataFilling.py
  */ 
+
+char** line2Arr(char* line){
+    // line[indexOf(line, '\n')] = '\0'; // strip the newline char
+    char** output = malloc();
+    char* token = strtok(line, ',');
+    /* walk through other tokens */
+    while( token != NULL && token != "\n") {
+        printf( " %s\n", token );
+    
+        token = strtok(NULL, s);
+   }
+}
 
 int main(){
     FILE* new_file = fopen("./data/fullData.csv", "w+");
@@ -13,6 +25,18 @@ int main(){
     int i = 0;
     char* line;
     while(fgets(line, 512, file) != NULL){
+        char* newline[10];
+        char** lineArr;
+        lineArr = line2Arr(line);
         
+
+
+
+        fprintf(new_file, "\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\n",
+            newline[0],newline[1],newline[2],newline[3],newline[4],
+            newline[5],newline[6],newline[7],newline[8],newline[9]);
+        i++;
     }
+    fclose(file);
+    fclose(new_file);
 }
