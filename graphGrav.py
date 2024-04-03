8121023dd79a7fb26ca1c7f69300b28799a8ef7f
Nick <nickford185@gmail.com>
2024-04-03 11:01:48 -0400

    added calculation of one asteroid's acceleration

--- graphGrav.py
+++ graphGrav.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt
 import math
+from numpy import interp
+from scipy.optimize import minimize
 
 DONUT_RADIUS = 0.2 # AU
 
-"""Gets the squared distance from start to end at the i-th time step"""
-def getDistSquare(start, end, i):
-    return (end[0][i]-start[0][i])**2 + (end[1][i]-start[1][i])**2 + (end[2][i]-start[2][i])**2
+"""Gets the squared distance from start to end"""
+def getDistSquare(start, end):
+    return (end[0]-start[0])**2 + (end[1]-start[1])**2 + (end[2]-start[2])**2
 
-"""Gets the unit vector from start toward end at the i-th time step"""
-def getDir(start, end, i):
+"""Gets the unit vector from time i in start toward time j in end"""
+def getDir(start, end, i, j):
     mag = math.sqrt(getDistSquare(start, end, i))
-    return [(end[0][i]-start[0][i])/mag, (end[1][i]-start[1][i])/mag, (end[2][i]-start[2][i])/mag]
+    return [(end[0][j]-start[0][i])/mag, (end[1][j]-start[1][i])/mag, (end[2][j]-start[2][i])/mag]
 
 """Retrieves a random mass based on the distributions of asteroid density and asteroid diameter"""
 def getMass():
     return 
 
-
+def f(t, args):
+    t1, t2 = t
+    lisa, asteroid, time = args
+    # print("\n\nCheck: ", str(len(time)), str(len(asteroid[0])), str(len(lisa[0])))
+    interpAster = [interp(t2, time, asteroid[0]),interp(t2, time, asteroid[1]),interp(t2, time, asteroid[2])]
+    interpLisa = []
+    for i in range(3):
+        interpLisa.append([interp(t1, time, lisa[i][0]),interp(t2, time, lisa[i][1]),interp(t2, time, lisa[i][2])])
+    return min(getDistSquare(interpLisa[0], interpAster), getDistSquare(interpLisa[1], interpAster), getDistSquare(interpLisa[2], interpAster))
 # import LISA data
 filename = "export_lisa"
 with open("./plots/" + filename + ".csv") as file:
@@ -39,29 +49,36 @@ with open("./plots/" + filename + ".csv") as file:
     data = file.readlines()
     data_list = []
     pos_aster = [[],[],[]] # pos[x_i][~t]
+    time = []
     for i in range(len(data)):
         data_list.append(data[i].strip("\n").split(","))
-    print(len(data_list),len(data_list[0]))
+    # print(len(data_list),len(data_list[0]))
     for i in range(len(data_list)):
+        time.append(float(i)/365.0/24.0)
         for j in range(len(data_list[i])):
             pos_aster[j%3].append(float(data_list[i][j]))
 
+
 # calculate acceleration
 accel = []
-M=1
-if(len(pos_aster[0]) == len(pos_lisa[0][0])):
-    for i in range(len(pos_aster[0])):
-        minDistSquare = min(getDistSquare(pos_lisa[0], pos_aster, i),
-                    getDistSquare(pos_lisa[1], pos_aster, i),
-                    getDistSquare(pos_lisa[2], pos_aster, i))
-        # only save the value if it's in the donut
-        if (minDistSquare < DONUT_RADIUS**2):
-        # a_i from F = GMm/r^2 => a = GM/r^2 ; G=1 in our units
-            a_i = M/minDistSquare  
-            accel.append(a_i)
-else:
+M = 124.84912296080462
+if(not len(pos_aster[0]) == len(pos_lisa[0][0])):
     # Show error message
     print("Array lengths do not match: array[" + str(len(pos_aster[0])) + "] and array[" + str(len(pos_lisa[0][0])) + "] found.")
+    
+else:
+    # for i in range(len(pos_aster[0])):
+    result = minimize(f, [0,0], args=[pos_lisa, pos_aster, time])
+    print(result.x)
+    # only save the value if it's in the donut
+    # if (minDistSquare < DONUT_RADIUS**2):
+    if(not result.success):
+        print("Error:" + str(result.message))
+    else:
+        # a_i from F = GMm/r^2 => a = GM/r^2 ; G=1 in our units
+        a_i = M/result.x[1]
+        accel.append(a_i)
+print(accel)
 
 """
 
@@ -71,7 +88,7 @@ for volume make histogram of known ones then pull other asteroid volumes out of 
 
 Do we want to average the acceleration felt by the three spacecraft?
     (a0 + a1 + a2)/3
-Or the closest spacecraft to the asteroid?
+Or the closest spacecraft to the asteroid? <<< this
     a_minDist
     - possibly this ( confirm with shane)
 
